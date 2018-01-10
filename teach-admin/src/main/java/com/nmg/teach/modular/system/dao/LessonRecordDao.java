d22e81e4921b5524c4fdbfe7444a1888dd39bb3b
夏鼎力 <xiadingli@wdai.com>
2018-01-10 20:17:51 +0800

    上课记录

---
+++ teach-admin/src/main/java/com/nmg/teach/modular/system/dao/LessonRecordDao.java
@@ -0,0 +1,26 @@
+package com.nmg.teach.modular.system.dao;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 管理员的dao
+ *
+ * @author xiadingli
+ * @date 2017年2月12日 下午8:43:52
+ */
+public interface LessonRecordDao {
+
+
+    /**
+     * 根据条件查询用户列表
+     *
+     * @return
+     * @date 2017年2月12日 下午9:14:34
+     */
+    List<Map<String, Object>> selectRecord(@Param("name") String name);
+
+
+}
